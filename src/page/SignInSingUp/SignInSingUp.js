779e09ec643899474ade1aab1d69a46fed34fd50
galileoluna <galileoungs@gmail.com>
2020-08-17 22:30:44 -0300

    genere la estructura del componente izq

--- src/page/SignInSingUp/SignInSingUp.js
+++ src/page/SignInSingUp/SignInSingUp.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { Container, Row ,Col, Button} from "react-bootstrap";
 
+import LogoWhiteTwittor from "../../assets/png/logo-white.png";
+import LogoTwittor from "../../assets/png/logo.png";
+
+import "./SignInSingUp.scss";
 export default function SignInSingUp() {
     return (
         //Fluid es para que ocupe toda la pagina
@@ -17,10 +21,22 @@ export default function SignInSingUp() {
 function LeftComponent(){
     return (
 
-        <Col className="signin-signup__left">
-            <h2>
-                LeftComponent...
-            </h2>
+        <Col className="signin-signup__left" xs={6}>
+          <img src={LogoTwittor} alt="Twittor" />
+          <div>
+
+              <h2>
+                  -Sigue lo que te interesa
+              </h2>
+
+              <h2>
+                  -Enterate de que esta pasando
+              </h2>
+              
+              <h2>
+                  -Unete 
+              </h2>
+          </div>
         </Col>
     )
 
@@ -31,7 +47,7 @@ function LeftComponent(){
 function RightComponent(){
     return (
 
-        <Col className="signin-signup__right">
+        <Col className="signin-signup__right" xs={6}>
             <h2>
                 RightComponent...
             </h2>
